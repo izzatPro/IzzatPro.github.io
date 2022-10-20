f00ba2e8e03b9d8fb9a3985e834953b11d011e11
Izzat <superizzat564@gmail.com>
2022-10-20 19:00:22 +0500

    update to move effect on the header

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
 window.addEventListener('load',function(){
+        //Mouse Circle
     const mouseCircle = document.querySelector('.mouse-circle');
     const mouseDot = document.querySelector('.mouse-dot');
-    //Mouse Circle
+
     const mouseCircleFn = (x,y) => {
         mouseCircle.style.cssText = `top:${y}px;left:${x}px;opacity:1;`;
         mouseDot.style.cssText = `top:${y}px;left:${x}px;opacity:1;`;
     };
+        //End of Mouse Circle
+        //Animated Circles
+    const circles = document.querySelectorAll('.circle');
+    const mainImg = document.querySelector('.main-circle img');
+
+    let mX = 0;
+    let mY = 0;
+
+    const animateCircles = (e,x,y) =>{
+        if(x < mX){
+            circles.forEach( (circle) => {
+                circle.style.left = `100px`;
+            });
+            mainImg.style.left = `-100px`;
+        } else if (x > mX){
+            circles.forEach( (circle) => {
+                circle.style.left = `-100px`;
+            });
+            mainImg.style.left = `-350px`;
+        }
+
+
+        if( y < mY){
+            circles.forEach( (circle) => {
+                circle.style.top = `100px`;
+            });
+            mainImg.style.top = `-400px`;
+        } else if ( y > mY ) {
+            circles.forEach( (circle) => {
+                circle.style.top = `-100px`;
+            });
+            mainImg.style.top = `-200px`;
+        }
+
+        mX = e.clientX;
+        mY = e.clientY;
+    };
+        //End of Animated Circles
+
     document.body.addEventListener('mousemove', function (e) {
         let x = e.clientX;
         let y = e.clientY;
         mouseCircleFn(x,y);
+        animateCircles(e,x,y);
     });
     
     document.body.addEventListener('mouseleave', function (e) {
@@ -17,4 +58,5 @@ window.addEventListener('load',function(){
       mouseDot.style.opacity = 0;
     
     });
+
 });
