fbf70ea7bec13e51f42f7b0a25298eb597cfbc44
Izzat <superizzat564@gmail.com>
2022-10-27 19:45:40 +0500

    Ended projects section without show more, i created close button and scroll part

--- script.js
+++ script.js
@@ -96,7 +96,9 @@ window.addEventListener('load',function(){
     // End of About Me Text
 
     // Projects
+    const container = document.querySelector('.container');
     const projects = document.querySelectorAll('.project');
+    const projectHideBtn = document.querySelector('.project-hide-btn');
     projects.forEach((project)=>{
         project.addEventListener('mouseenter', () => {
             project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20}px`;
@@ -104,6 +106,28 @@ window.addEventListener('load',function(){
         project.addEventListener('mouseleave', () =>{
             project.firstElementChild.style.top = "2rem";
         });
+
+        //Big Project
+        project.addEventListener('click',() =>{
+            const bigImgWrapper = document.createElement("div");
+            bigImgWrapper.className = "project-img-wrapper";
+            container.appendChild(bigImgWrapper);
+            const bigImg =  document.createElement("img");
+            bigImg.className = "project-img";
+            const imgPath = project.firstElementChild.getAttribute("src").split('.')[0];
+            bigImg.setAttribute("src",`${imgPath}-big.jpg`);
+            bigImgWrapper.appendChild(bigImg);
+            document.body.style.overflowY = "hidden";
+
+
+            projectHideBtn.classList.add("change");
+            projectHideBtn.onclick = () => { 
+            projectHideBtn.classList.remove("change");
+            bigImgWrapper.remove();
+            document.body.style.overflowY = "scroll";
+            } ;
+        });
+        //End of //Big Project
     });
    
     // End of Projects
