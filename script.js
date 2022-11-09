6db07b3be60715d81df5b62f48052097ee8c60a8
Izzat <superizzat564@gmail.com>
2022-11-09 16:45:15 +0500

    Slider is finished

--- script.js
+++ script.js
@@ -241,5 +241,26 @@ window.addEventListener('load',function(){
         });
     });
     // End of Form
+
+    // Slideshow
+    const slideshow = document.querySelector('.slideshow');
+    setInterval(() => {
+        const firstIcon = slideshow.firstElementChild;
+        firstIcon.classList.add('faded-out');
+        const thirdIcon = slideshow.children[3];
+        thirdIcon.classList.add('light');
+        thirdIcon.previousElementSibling.classList.remove('light')
+        setTimeout(()=>{
+            slideshow.removeChild(firstIcon);
+            slideshow.appendChild(firstIcon);
+            setTimeout(() =>{
+                firstIcon.classList.remove('faded-out');
+            }, 500);
+         
+        }, 500);
+      
+
+    }, 3000);
+    // End of Slideshow
     // End of Section 5
 });
